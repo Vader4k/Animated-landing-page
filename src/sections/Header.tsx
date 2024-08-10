e62005da6246259c500483076f4d5efc42b44a0d
danie odukwe <kingdanny295@gmail.com>
2024-08-10 11:39:40 +0100

    moved inages to src directory so it can be accessed as a component

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,12 @@
-export const Navbar = () => {
-  return null;
+import ArrowRight from '@/assets/arrow-right.svg';
+
+export const Header = () => {
+  return (
+    <nav>
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm">
+        <p>Get started for free</p>
+        <ArrowRight />
+      </div>
+    </nav>
+  )
 };
